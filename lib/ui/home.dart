51084cbfe7e524621ac4f63e116f73539a69177f
yeojunyeop <popcorn37375@gmail.com>
2024-09-24 00:23:09 +0900

    url_launcher 라이브러리 추가 및 광고 배너 표시 UI 및 기능 작업

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:yeojunyeop_acote/view_model/home_view_model.dart';
@@ -16,6 +17,7 @@ class _HomeState extends State<Home> {
     HomeViewModel.to.getUser();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
@@ -36,54 +38,73 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: GetBuilder<HomeViewModel>(
-        builder: (_) {
-          return RawScrollbar(
-            thumbColor: const Color(0xff8a8a8c),
-            thickness: 3,
-            radius: const Radius.circular(20),
-            crossAxisMargin: 3,
-            child: ListView.builder(
-              itemCount: HomeViewModel.to.userList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    Container(
-                      padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                      color: const Color(0xff17181a),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 22,
-                            backgroundColor: Colors.blue,
-                            backgroundImage: NetworkImage(
-                                HomeViewModel.to.userList[index].avatar_url!),
-                          ),
-                          const SizedBox(width: 12),
-                          Text(HomeViewModel.to.userList[index].login!,
-                              style: const TextStyle(
-                                  color: Color(0xff989aa6), fontSize: 16)),
-                        ],
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        if(HomeViewModel.to.userList.length != index)
-                          Container(
-                              color: const Color(0xff17181a),
-                              height: 1,
-                              width: 72),
-                        Expanded(
-                            child: Container(
-                                color: const Color(0xff202125), height: 1)),
-                      ],
-                    )
-                  ],
+        builder: (_) => RawScrollbar(
+          thumbColor: const Color(0xff8a8a8c),
+          thickness: 3,
+          radius: const Radius.circular(20),
+          crossAxisMargin: 3,
+          child: ListView.builder(
+            itemCount: HomeViewModel.to.userList.length,
+            itemBuilder: (BuildContext context, int index) {
+              if (index % 10 == 9) {
+                return InkWell(
+                  onTap: () => HomeViewModel.to.launchAdUrl(),
+                  child: Image.network(
+                    'https://placehold.it/500x100?text=ad',
+                    height: 69,
+                    fit: BoxFit.cover,
+                    frameBuilder: (context, child, frame, sync) {
+                      if (frame == null) {
+                        return Container(
+                            color: const Color(0xffcccccc),
+                            height: 69,
+                            child: const Center(
+                                child: CupertinoActivityIndicator(
+                                    color: Color(0xFF9c9c9c))));
+                      }
+                      return child;
+                    },
+                  ),
                 );
-              },
-            ),
-          );
-        },
+              }
+              return Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 12),
+                    color: const Color(0xff17181a),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 22,
+                          backgroundColor: const Color(0xff202125),
+                          backgroundImage: NetworkImage(
+                              HomeViewModel.to.userList[index].avatar_url!),
+                        ),
+                        const SizedBox(width: 12),
+                        Text(HomeViewModel.to.userList[index].login!,
+                            style: const TextStyle(
+                                color: Color(0xff989aa6), fontSize: 16)),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      if (HomeViewModel.to.userList.length - 1 != index)
+                        Container(
+                            color: const Color(0xff17181a),
+                            height: 1,
+                            width: 72),
+                      Expanded(
+                          child: Container(
+                              color: const Color(0xff202125), height: 1)),
+                    ],
+                  )
+                ],
+              );
+            },
+          ),
+        ),
       ),
     );
   }
--- lib/view_model/home_view_model.dart
+++ lib/view_model/home_view_model.dart
@@ -1,13 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:yeojunyeop_acote/model/user_model.dart';
-
-import '../provider/functions.dart';
+import 'package:yeojunyeop_acote/provider/functions.dart';
 
 class HomeViewModel extends GetxController {
   static HomeViewModel get to => Get.find<HomeViewModel>();
 
   List<UserModel> userList = [];
+  LaunchMode launchMode = LaunchMode.platformDefault;
+
+  @override
+  void onInit() async {
+    super.onInit();
+    if (Platform.isAndroid) {
+      launchMode = LaunchMode.externalApplication;
+    }
+  }
 
   Future<void> getUser() async {
     try {
@@ -16,5 +27,10 @@ class HomeViewModel extends GetxController {
     } catch (e) {
       debugPrint('errorMessage: $e');
     }
+  }
+
+  Future<void> launchAdUrl() async {
+    Uri url = Uri.parse('https://taxrefundgo.kr');
+    launchUrl(url, mode: launchMode);
   }
 }
